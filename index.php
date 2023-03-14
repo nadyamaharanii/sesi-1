98e8e1441427ab90bbdc0fcabc4f990ce603164f
nadyamaharanii <127836850+nadyamaharanii@users.noreply.github.com>
2023-03-14 17:03:28 +0800

    sesi 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan html sesi 1</title>
+</head>
+<body>
+    
+    <h3>Belajar web programing<?="<sup>80</sup>"?></h3>
+
+    <script>
+        document.write("<h3>Menggunakan Script javascript</h3>");
+    </script>
+
+    <?php
+        echo "<h3>Menggunakan Script PHP</h3>";
+    ?>
+
+</body>
+</html>
